f8dc68e1e6ed81d9bab7174a7f65fc5037286659
Alexander Entinger <cto@lxrobotics.com>
2020-03-23 13:28:31 +0100

    Restructuring for better readability

--- src/BMP388/BMP388_Config.cpp
+++ src/BMP388/BMP388_Config.cpp
@@ -32,32 +32,44 @@ BMP388_Config::BMP388_Config(BMP388_Io & io)
 
 void BMP388_Config::configIntPinOutputType(IntPinOutputType const type)
 {
-  _io.modify(Register::INT_CTRL, bm(INT_CTRL::INT_OD), to_integer(type));
+  _io.modify(Register::INT_CTRL,
+             bm(INT_CTRL::INT_OD),
+             to_integer(type));
 }
 
 void BMP388_Config::configIntPinLevel(IntPinLevel const level)
 {
-  _io.modify(Register::INT_CTRL, bm(INT_CTRL::INT_LEVEL), to_integer(level));
+  _io.modify(Register::INT_CTRL,
+             bm(INT_CTRL::INT_LEVEL),
+             to_integer(level));
 }
 
 void BMP388_Config::enableDataReadyInt()
 {
-  _io.modify(Register::INT_CTRL, bm(INT_CTRL::DRDY_EN), bm(INT_CTRL::DRDY_EN));
+  _io.modify(Register::INT_CTRL,
+             bm(INT_CTRL::DRDY_EN),
+             bm(INT_CTRL::DRDY_EN));
 }
 
 void BMP388_Config::enablePressure()
 {
-  _io.modify(Register::PWR_CTRL, bm(PWR_CTRL::PRESS_EN), bm(PWR_CTRL::PRESS_EN));
+  _io.modify(Register::PWR_CTRL,
+             bm(PWR_CTRL::PRESS_EN),
+             bm(PWR_CTRL::PRESS_EN));
 }
 
 void BMP388_Config::enableTemperature()
 {
-  _io.modify(Register::PWR_CTRL, bm(PWR_CTRL::TEMP_EN), bm(PWR_CTRL::TEMP_EN));
+  _io.modify(Register::PWR_CTRL,
+             bm(PWR_CTRL::TEMP_EN),
+             bm(PWR_CTRL::TEMP_EN));
 }
 
 void BMP388_Config::configPowerMode(PowerMode const mode)
 {
-  _io.modify(Register::PWR_CTRL, to_integer(PowerMode::Normal), to_integer(mode));
+  _io.modify(Register::PWR_CTRL,
+             bm(PWR_CTRL::MODE_1) | bm(PWR_CTRL::MODE_0),
+             to_integer(mode));
 }
 
 /**************************************************************************************
