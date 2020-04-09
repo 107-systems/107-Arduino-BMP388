871c3a49fed29d2ba0a2cb5f285166c31853be1b
Alexander Entinger <cto@lxrobotics.com>
2020-04-09 06:51:59 +0200

    Changing output format for better readability in fast moving data stream

--- examples/BMP388-Basic/BMP388-Basic.ino
+++ examples/BMP388-Basic/BMP388-Basic.ino
@@ -89,8 +89,8 @@ void onExternalEvent()
 
 void onSensorData(double const pressure_hpa, double const temperature_deg)
 {
-  Serial.print("Barometric Pressure [hPa] = ");
-  Serial.println(pressure_hpa);
-  Serial.print("Temperature [°C]          = ");
-  Serial.println(temperature_deg);
+  Serial.print(pressure_hpa);
+  Serial.print(" hPa / ");
+  Serial.print(temperature_deg);
+  Serial.println(" °C");
 }
